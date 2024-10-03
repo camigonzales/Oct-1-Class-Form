a816603b8e205814e798efc8955e1daf44f707b4
camigonzales <camileighgonzales@gmail.com>
2024-10-03 14:07:27 -0500

    class work oct 3- java

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
 // JS scripts placed here
+
+const littlebutton = document.getElementById("submit");
+console.log(littlebutton);
+
+littlebutton.addEventListener("click", (ev) => {
+    ev.preventDefault();
+})
+
+const element = document.getElementsByTagName("p");
+console.log(element);
+
+const x = document.querySelectorAll(".logo");
+console.log(x);
+
+const y = document.querySelector(".logo"); //Use this one (most of the time)!!!!
+console.log(y);
+
